861993f1f9a77c6cbe583b3830f04f6817a46371
AHolcsik <annamaria.holcsik@gmail.com>
2017-09-28 12:24:39 +0200

    feat(todo_app): main controller added, print message when unsuported argument is given

--- todo_app.py
+++ todo_app.py
@@ -3,9 +3,25 @@ import sys
 def app_arg():
     return len(sys.argv)
 
-def todo_controller():
+def todo_print():
     if app_arg() == 1:
         print('Command Line Todo application \n ============================= \n Command line arguments: \n -l   Lists all the tasks \n -a   Adds a new task \n -r   Removes an task \n -c   Completes an task')
+    else:
+        return sys.argv[1]
+
+def todo_controller():
+    if sys.argv[1] == '-l':
+        pass
+    elif sys.argv[1] == '-a':
+        pass
+    elif sys.argv[1] == '-r':
+        pass
+    elif sys.argv[1] == '-c':
+        pass
+    else:
+        print('Unsuported argument')
+
+
 
 todo_controller()
 
